746ee31471c7b2b5c9fd3824aa3e895fc57a099b
JP-Bailey <josiahpbailey@gmail.com>
2026-02-05 20:51:20 -0500

    Basic functionality

--- iMEC_ThePriceisDice.ino
+++ iMEC_ThePriceisDice.ino
@@ -18,14 +18,19 @@
 //global(s)
 //
 unsigned int button_status = 0;
+unsigned int count = 0;
 
 Servo wheel_servo;
 
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(13, INPUT);
   //Setup Servo ---------------------------------------------------------------
   wheel_servo.attach(9);       //attaches the servo on pin 9 to the servo object
+
+  Serial.begin(9600);
 
 }
 
@@ -35,11 +40,18 @@ void loop() {
   //Read the status of the spin configuration switches. 
 
   //Read the status of the button. 
+  button_status = digitalRead(13);
 
   //Only start a spin if the button has been pressed. 
   if (button_status == 1)
   {
+    Serial.println("Hello Addie");
 
+    wheel_servo.write(90);
   }
-
+  else
+  {
+    wheel_servo.write(0);
+  }
+  
 }
