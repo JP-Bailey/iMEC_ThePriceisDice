1fd558682f0454a437efd26abdab9c2f7424c537
JP-Bailey <josiahpbailey@gmail.com>
2026-02-21 22:17:40 -0500

    added selectable auto mode - bramer

--- iMEC_ThePriceisDice.ino
+++ iMEC_ThePriceisDice.ino
@@ -8,29 +8,73 @@
 //Includes 
 #include <Servo.h>
 
+#define BUTTON 13 
 
 //
 //global(s)
 //
- //Var to store the value of the button
+
+//Var to store the value of the button
+
+int button_value = 0 ;
+
+int mode = 0;
 
 Servo wheel_servo;
 
 void setup() {
   // put your setup code here, to run once:
 
+  //Configuring the button 
+  pinMode(BUTTON, INPUT);
+
+  //Read the intial condition of the button 
+
+  button_value = digitalRead(BUTTON);
+
+  //check the buttons value if it is a 1 set system to auto run if 0 set to manual
+
+  if (button_value == 1){
+    mode = 1;
+  }
+  else{
+    mode = 0;
+  }
+
   //Setup Servo ---------------------------------------------------------------
-         //attaches the servo to the servo object
+  wheel_servo.attach(9); //attaches the servo to the servo object
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
+  //Read the status of the button.
+  button_value = digitalRead(BUTTON);
 
-  //Read the status of the spin configuration switches. 
+  //Check the mode 
+  if (mode == 1){
+  //Auto Mode
+  if (button_value == 1){
+    wheel_servo.write(80);
+    delay(6000);
+    wheel_servo.write(0);
+  }
+  }
+  else if (mode == 0){
+    //Manual 
+        if (button_value == 1){
+          wheel_servo.write(80);
+        }
+        else{
+          wheel_servo.write(0);
+        }
+      delay(100);
+  }
 
-  //Read the status of the button. 
 
+
+  
   //Only start a spin if the button has been pressed. 
 
 }
